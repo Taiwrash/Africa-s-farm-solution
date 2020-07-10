412d232b88d47617fd19e6dc534a53b225b4011a
Taiwrash <rasheedrtm1@gmail.com>
2020-07-10 10:15:44 +0100

    server correction

--- afs-frontend/src/Components/Pages/Payment.js
+++ afs-frontend/src/Components/Pages/Payment.js
@@ -22,7 +22,7 @@ const Payment = () => {
   };
 
   useEffect(() => {
-    const Api = `https://frozen-peak-27970.herokuapp.com//api/products/_id`;
+    const Api = `https://frozen-peak-27970.herokuapp.com//api/products/`;
     // const Api = "http://localhost:4000/api/products/5efae3a08f8edf1fe42a2c7d";
     fetch(Api)
       .then((res) => res.json())
@@ -38,7 +38,7 @@ const Payment = () => {
 
     amount: info.price * 100,
     publicKey: "pk_test_42b9a5dd2aced92bd955960c7a4741b819ba9e62",
-     //secretKey: "sk_test_f7965348cd3f27e7a90ae01dc7174373d8c62586",
+    //secretKey: "sk_test_f7965348cd3f27e7a90ae01dc7174373d8c62586",
   };
 
   const initializePayment = usePaystackPayment(config);
